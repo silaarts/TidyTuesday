afdfa7d5f66c8710aaf7218a08d53fec1a00cca2
Sil Aarts <47814058+silaarts@users.noreply.github.com>
2019-06-07 13:51:00 +0200

    Update TidyTuesday_Teachersratio.R

--- TidyTuesday_Teachersratio.R
+++ TidyTuesday_Teachersratio.R
@@ -1,5 +1,5 @@
 #TidyTuesday
-#Student-teacher ratio teaching
+#Pupil-teacher ratio
 
 #Load libraries
 library(dplyr)
@@ -13,6 +13,7 @@ library(tidytext)
 library(stringr)
 library(gganimate)
 library(extrafont)
+library(LaCroixColoR)
 
 #Read file
 data <- read.csv2("Desktop/Data1.csv", stringsAsFactors = F)
@@ -31,7 +32,7 @@ colnames(countries) [1] <- c("country")
 countries$ID <- seq.int(nrow(countries))
 
 #API key google
-register_google(key = "Enter your key here")
+register_google(key = "Your key here")
 
 #Make the geocode data.frame
 europe <- geocode(c("Austria", "Belgium", "Bulgaria", "Cyprus", "Czechia", "Denmark", "Estonia","Finland","France", "Germany", "Greece", "Hungary", "Ireland",
@@ -41,10 +42,10 @@ europe$ID <- seq.int(nrow(europe))
 
 #Combine the two datafiles
 data3 <- merge(x = europe, y = countries, by = "ID", all = TRUE)
-#Combine with original file
+#Combine with original file regarding the grading
 data4 <- merge(x = data3, y = data1, by = "country", all = TRUE)
 
-#Delete last decimals for ease of use
+#Delete last 4 decimals
 data4$grade <- substr(data4$grade, 1, nchar(data4$grade)-4)
 
 #Make year = date
@@ -54,23 +55,25 @@ data4$grade  <- as.numeric(gsub('![[:alnum:]]*[[:space:]]|[[:punct:]]', '', data
 data4$grade <- data4$grade/10.
 
 #Select 2016
-data6 <- data5 %>%
-  filter(year=="2016-05-07")
+data5 <- data4 %>%
+  filter(year=="2016-06-07")
 
+#Ready to make the ggmap
 #Get ggmap Europe
 europe_map<- get_map(location='Europe', zoom=4)
 
-#Ready to make the ggmap
+#GGplot
 p <- ggmap(europe_map)+
-  geom_point(data = data6, aes(x = lon, y = lat, color = grade), size=5) + 
-  scale_colour_gradient(name = 'Ratio', low="turquoise1", high="slateblue")+
-  labs(title ="TidyTuesday: Tertiary education 2016", 
-       subtitle="Pupil-teacher ratio in tertiary education", 
+  geom_point(data = data5, aes(x = lon, y = lat, color = grade), size=5)+
+  scale_colour_gradient(name = 'Ratio', low="blue", high="darkorange1")+
+  labs(title ="TidyTuesday: Tertiary education", 
+       subtitle="Pupil-teacher ratio in 2016 (grey: missing data)", 
        caption="Source: UNESCO | Plot by: @sil_aarts")+
   theme(
-        plot.title= element_text(family="Times New Roman", face="bold", size=18),
-        plot.subtitle= element_text(family="Times New Roman", size=16),
-        plot.caption= element_text(family="Times New Roman", size=12),
+        text=element_text(family="mono"),
+        plot.title= element_text(face="bold", size=18),
+        plot.subtitle= element_text(size=16),
+        plot.caption= element_text(size=11),
         axis.text.x=element_blank(),
         axis.text.y=element_blank(),
         axis.title.y = element_blank(), 

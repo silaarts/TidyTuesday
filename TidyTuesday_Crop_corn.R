b1391aa52f64447b043a134e08c52d09084e9102
Sil Aarts <47814058+silaarts@users.noreply.github.com>
2020-09-01 08:17:00 +0200

    Update and rename TidyTuesday_Crop_corn to TidyTuesday_Crop_corn.R

--- TidyTuesday_Crop_corn.R
+++ TidyTuesday_Crop_corn.R
@@ -21,6 +21,8 @@ colnames(crop)[6] <- "Maize"
 crop$Entity[crop$Entity == "Bosnia and Herzegovina"] <- "Bosnia &\nHerzegovina"
 crop$Entity[crop$Entity == "Saint Vincent and the Grenadines"] <- "Saint Vincent\n&\nGrenadines"
 crop$Entity[crop$Entity == "United Arab Emirates"] <- "United Arab\nEmirates"
+crop$Entity[crop$Entity == "New Caledonia"] <- "New\nCaledonia"
+
 
 #Select
 data <- crop %>%
@@ -29,8 +31,7 @@ data <- crop %>%
   drop_na(Code)%>%
   top_n(50, Maize)
 
-#Choose some nice fonts
-font_add_google("Fugaz One", "F")
+#Choose sme nice fonts
 font_add_google("Calligraffitti", "P")
 font_add_google("Bad Script", "B")
 showtext_auto()
@@ -38,12 +39,12 @@ showtext_auto()
 #Make a flower
 p2 <- ggplot(data, aes(fill=Entity, alpha = Maize)) +
   #Corn
-  geom_ellipse(aes(x0 = 0, y0 = 9, a = 7, b = 3, angle = pi / 2, m1 = 3), fill= "gold3",  color = "gold4") + #top
+  geom_ellipse(aes(x0 = 0, y0 = 9, a = 7, b = 3, angle = pi / 2, m1 = 3), fill= "gold3",  color = "coral4") + 
   #Leaves
-  geom_ellipse(aes(x0 = 6, y0 = 6.2, a = 5, b = 1, angle = pi / 4, m1 = 1), fill="olivedrab4",  color = "transparent", alpha=1) + #dia_top_right
-  geom_ellipse(aes(x0 = 6.2, y0 = 9.2, a = 3, b = 1, angle = pi / 3, m1 = 1), fill="olivedrab4",  color = "transparent", alpha=1) + #dia_top_left
-  geom_ellipse(aes(x0 = -6, y0 = 6.2, a = 5, b = 1, angle = 3*pi / 4, m1 = 1), fill="olivedrab4",  color = "transparent", alpha=1) + #dia_top_left
-  geom_ellipse(aes(x0 = -6.2, y0 = 9.2, a = 3, b = 1, angle = 2*pi / 3, m1 = 1), fill="olivedrab4",  color = "transparent", alpha=1) + #dia_top_left
+  geom_ellipse(aes(x0 = 6.1, y0 = 6.1, a = 5, b = 1, angle = pi / 4, m1 = 1), fill="olivedrab4",  color = "transparent", alpha=1) + 
+  geom_ellipse(aes(x0 = 6.2, y0 = 9.2, a = 3, b = 1, angle = pi / 3, m1 = 1), fill="olivedrab4",  color = "transparent", alpha=1) + 
+  geom_ellipse(aes(x0 = -6.1, y0 = 6.1, a = 5, b = 1, angle = 3*pi / 4, m1 = 1), fill="olivedrab4",  color = "transparent", alpha=1) +
+  geom_ellipse(aes(x0 = -6.2, y0 = 9.2, a = 3, b = 1, angle = 2*pi / 3, m1 = 1), fill="olivedrab4",  color = "transparent", alpha=1) + 
   facet_wrap(~ Entity, nrow=5, ncol=10)+
   theme_void()+
   theme(
@@ -58,16 +59,18 @@ p2 <- ggplot(data, aes(fill=Entity, alpha = Maize)) +
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     plot.margin = unit(c(1,1,1,1), "cm"))
+#Run it!
+p2
 
 #Create a legend
 legend <- ggplot(data) +
   #Corn
-  geom_ellipse(aes(x0 = 0, y0 = 10, a = 7, b = 3, angle = pi / 2, m1 = 3), fill= "gold3",  color = "coral4") + #top
+  geom_ellipse(aes(x0 = 0, y0 = 10, a = 7, b = 3, angle = pi / 2, m1 = 3), fill= "gold3",  color = "coral4") + 
   #Leaves
-  geom_ellipse(aes(x0 = 5.8, y0 = 6.2, a = 5, b = 1, angle = pi / 4, m1 = 1), fill="olivedrab4",  color = "transparent") + #dia_top_right
-  geom_ellipse(aes(x0 = 6.2, y0 = 10.2, a = 4, b = 1, angle = pi / 3, m1 = 1), fill="olivedrab4",  color = "transparent") + #dia_top_left
-  geom_ellipse(aes(x0 = -5.8, y0 = 6.2, a = 5, b = 1, angle = 3*pi / 4, m1 = 1), fill="olivedrab4",  color = "transparent") + #dia_top_left
-  geom_ellipse(aes(x0 = -6.2, y0 = 10.2, a = 4, b = 1, angle = 2*pi / 3, m1 = 1), fill="olivedrab4",  color = "transparent") + #dia_top_left
+  geom_ellipse(aes(x0 = 5.7, y0 = 6.2, a = 5, b = 1, angle = pi / 4, m1 = 1), fill="olivedrab4",  color = "transparent") + 
+  geom_ellipse(aes(x0 = 6.1, y0 = 10.2, a = 4, b = 1, angle = pi / 3, m1 = 1), fill="olivedrab4",  color = "transparent") + 
+  geom_ellipse(aes(x0 = -5.7, y0 = 6.2, a = 5, b = 1, angle = 3*pi / 4, m1 = 1), fill="olivedrab4",  color = "transparent") + 
+  geom_ellipse(aes(x0 = -6.1, y0 = 10.2, a = 4, b = 1, angle = 2*pi / 3, m1 = 1), fill="olivedrab4",  color = "transparent") + 
   #Mark
   geom_mark_circle(aes(x=0, y = 15, label = "A lighter color is indicative for\nless tonnes per hectare"), label.fill=  "transparent", label.colour = "white", label.buffer = unit(20, "mm"), label.family = c("C"), expand = unit(1, "mm"), con.type="elbow", con.colour= "black") +
   labs(caption="Source: Global Crop Yields & Our World in Data | Plot by @sil_aarts")+
@@ -86,13 +89,17 @@ legend <- ggplot(data) +
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     plot.margin = unit(c(1,1,1,1), "cm"))
+    
+
+#Run it
+legend
 
 #Create title
 title <- ggplot() +
   #geom_hline(yintercept = 3.5, colour="black", size=2)+
   labs(x = NULL, y = NULL,
        title = "CORN",
-       subtitle = "Top 50 countries with the most tonnes of corn (maize) per hectares.")+
+       subtitle = "Top 50 countries with the most tonnes of corn (maize) per hectares in 2018")+
   theme_void(10)+
   theme(
     #text = element_text(family="C"),
